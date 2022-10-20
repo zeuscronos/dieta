a22981f836e8e3216721361f04527832c2c3813e
Zeuscronos <zeuscronos@gmail.com>
2022-10-20 18:41:55 +0200

    https://zeuscronos.github.io/dieta/?ids=1234,1235

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ export default [
   {
     id: '1234',
     description: 'Yogur sin lactosa natural Hacendado 0% m.g 0% sin azúcares añadidos',
-    link: 'https://store.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
+    link: 'https://tienda.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
     image: 'https://prod-mercadona.imgix.net/images/0d226d4379884475431e18647cf97f81.jpg?fit=crop&h=600&w=600',
     store: 'Mercadona',
     price: 1.40,
@@ -18,7 +18,7 @@ export default [
   {
     id: '1234',
     description: 'Yogur sin lactosa natural Hacendado 0% m.g 0% sin azúcares añadidos',
-    link: 'https://store.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
+    link: 'https://tienda.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
     image: 'https://prod-mercadona.imgix.net/images/0d226d4379884475431e18647cf97f81.jpg?fit=crop&h=600&w=600',
     store: 'Mercadona',
     price: 1.40,
@@ -34,7 +34,7 @@ export default [
   {
     id: '1234',
     description: 'Yogur sin lactosa natural Hacendado 0% m.g 0% sin azúcares añadidos',
-    link: 'https://store.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
+    link: 'https://tienda.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
     image: 'https://prod-mercadona.imgix.net/images/0d226d4379884475431e18647cf97f81.jpg?fit=crop&h=600&w=600',
     store: 'Mercadona',
     price: 1.40,
@@ -50,7 +50,7 @@ export default [
   {
     id: '1234',
     description: 'Yogur sin lactosa natural Hacendado 0% m.g 0% sin azúcares añadidos',
-    link: 'https://store.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
+    link: 'https://tienda.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
     image: 'https://prod-mercadona.imgix.net/images/0d226d4379884475431e18647cf97f81.jpg?fit=crop&h=600&w=600',
     store: 'Mercadona',
     price: 1.40,
@@ -66,7 +66,7 @@ export default [
   {
     id: '1234',
     description: 'Yogur sin lactosa natural Hacendado 0% m.g 0% sin azúcares añadidos',
-    link: 'https://store.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
+    link: 'https://tienda.mercadona.es/product/20164/yogur-sin-lactosa-natural-hacendado-0-mg-0-sin-azucares-anadidos-pack-4',
     image: 'https://prod-mercadona.imgix.net/images/0d226d4379884475431e18647cf97f81.jpg?fit=crop&h=600&w=600',
     store: 'Mercadona',
     price: 1.40,
